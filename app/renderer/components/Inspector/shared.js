3a4f7087d0ae53caba8aed83d7fa4f6b8a008dfe
VPsuraj <suraj@valuepitch.com>
2023-07-10 15:30:25 +0530

    disk and zoom out is comment out!

--- app/renderer/components/Inspector/shared.js
+++ app/renderer/components/Inspector/shared.js
@@ -132,7 +132,7 @@ export const SCREENSHOT_INTERACTION_MODE = {
   DOUBLE_TAP: 'double tap',
   GESTURE: 'gesture',
   ZOOMIN: 'zoomin',
-  ZOOMOUT: 'zoomout',
+  // ZOOMOUT: 'zoomout',
   SLIDE: 'slide'
 };
 
